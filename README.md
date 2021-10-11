0acd9fba2fff0a634412dadba12f9eaee3297e3f
Neko7soraBOT <75793267+Neko7sora@users.noreply.github.com>
2021-10-11 15:03:13 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# piping-server-rust-replit
+[Piping Server](https://github.com/nwtgck/piping-server-rust) written in Rust on Replit
---
+++ start.bash
@@ -0,0 +1,28 @@
+#! /bin/bash -xeu
+
+# Piping Server(Rust) version
+VERSION=v0.8.5
+# binary directory
+BIN_DIR_PATH=$PWD/bin/$VERSION
+BIN_PATH=$BIN_DIR_PATH/piping-server
+
+# Make directory for bin
+mkdir -p $BIN_DIR_PATH
+
+# If bin is not found
+if [ ! -f $BIN_PATH ]; then
+  (
+    # Go to the bin directory
+    cd $BIN_DIR_PATH
+    # Donwload and extract
+    curl -L https://github.com/nwtgck/piping-server-rust/releases/download/${VERSION}/piping-server-x86-64-linux.tar.gz | tar xzvf -
+    # Move
+    mv ./piping-server-x86-64-linux/piping-server $BIN_PATH
+    rm -rf piping-server-x86-64-linux/
+  )
+fi
+
+# Save PID
+echo $$ > piping-server.pid
+# Run a server
+exec $BIN_PATH --http-port=3000
